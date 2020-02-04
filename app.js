7b3fc66e14aa5d173831b8c6691e5bdc0b3e76d0
Derek Pauly <jayhawkfan8484@gmail.com>
2020-02-04 16:42:57 -0600

    finished logic, fixed bugs

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+"use strict";
+
 let myLibrary = [];
 myLibrary.push(new Book("art", "lauren", "433", "yes"));
 myLibrary.push(new Book("two", "cassie", "567", "no"));
-trialBook = new Book("three", "jace", "456", "no");
+let trialBook = new Book("three", "jace", "456", "no");
 myLibrary.push(trialBook);
 render();
 
@@ -12,10 +14,10 @@ function findBookfromDiv(myDiv) {
 
 
 //Event Listeners
-newBtn = document.querySelector('.new-btn')
+const newBtn = document.querySelector('.new-btn')
 newBtn.onclick = showForm;
 
-submitBtn = document.querySelector('#submit');
+const submitBtn = document.querySelector('#submit');
 submitBtn.onclick = addBookToLibrary;
 
 function Book(title, author, pages, read) {
@@ -27,6 +29,7 @@ function Book(title, author, pages, read) {
 
 Book.prototype.changeReadStatus = function () {
   this.read = (this.read === 'yes') ? 'no' : 'yes';
+  return;
 }
 
 function updateBook(e) {
@@ -35,24 +38,23 @@ function updateBook(e) {
     removeBook();
   }
   if (e.target.classList.contains('read-btn')) {
-    // console.log(e.target.parentNode.dataset.title);
-    div = (e.target.parentNode);
-    changeTitle = e.target.parentNode.dataset.title;
-    myBook = myLibrary.find(book => (book.title === changeTitle));
+    const div = (e.target.parentNode);
+    const changeTitle = e.target.parentNode.dataset.title;
+    const myBook = myLibrary.find(book => (book.title === changeTitle));
     myBook.changeReadStatus();
-    readPara = (div.querySelector('p:last-of-type'));
+    const readPara = (div.querySelector('p:last-of-type'));
     readPara.textContent = (`pages: ${myBook.read}`);
   }
 
 
   function removeBook() {
-    removeTitle = e.target.parentNode.dataset.title;
-    removeDiv = e.target.parentNode;
-    bookContainer = document.querySelector('.book-container');
+    const removeTitle = e.target.parentNode.dataset.title;
+    const removeDiv = e.target.parentNode;
+    const bookContainer = document.querySelector('.book-container');
 
     //remove from myLibrary
-    myBook = myLibrary.find(book => (book.title === removeDiv.dataset.title));
-    let spot = myLibrary.indexOf(myBook);
+    const myBook = myLibrary.find(book => (book.title === removeDiv.dataset.title));
+    const spot = myLibrary.indexOf(myBook);
     myLibrary.splice(spot, 1);
 
     //remove from book-container
@@ -61,28 +63,27 @@ function updateBook(e) {
 }
 
 function addBookToLibrary() {
-  titleBox = document.querySelector('input#title');
-  authorBox = document.querySelector('input#author');
-  pagesBox = document.querySelector('input#pages');
-  readBox = document.querySelector('input#read');
+  const titleBox = document.querySelector('input#title');
+  const authorBox = document.querySelector('input#author');
+  const pagesBox = document.querySelector('input#pages');
+  const readBox = document.querySelector('input#read');
 
 
-  newBook = new Book(titleBox.value, authorBox.value,
+  const newBook = new Book(titleBox.value, authorBox.value,
     pagesBox.value, readBox.value)
   myLibrary.push(newBook);
-  console.table(myLibrary);
   hideForm();
   render();
 }
 
 function showForm() {
-  formContainer = document.querySelector('.form-container');
+  const formContainer = document.querySelector('.form-container');
   formContainer.style.display = "block";
 
 }
 
 function hideForm() {
-  formContainer = document.querySelector('.form-container');
+  const formContainer = document.querySelector('.form-container');
   formContainer.style.display = "none";
 
 }
@@ -100,7 +101,7 @@ function render() {
     displayBook(book);
   })
 
-  bookDivs = document.querySelectorAll('.book');
+  const bookDivs = document.querySelectorAll('.book');
   bookDivs.forEach(div => {
     div.addEventListener('click', updateBook)
   })
@@ -112,41 +113,46 @@ function displayBook(book) {
   if (displayedBookTitles.includes(book.title)) return;
 
   // Create div for book
-  div = document.createElement('div');
+  const div = document.createElement('div');
   div.classList.add('book');
   div.dataset.title = book.title;
 
   // Create <h3> for book
-  h1 = document.createElement('h3');
+  const h1 = document.createElement('h3');
   h1.textContent = book.title;
   div.appendChild(h1);
 
   //create paragraph for each book property
-  for (property in book) {
-    para = document.createElement('p');
-    para.classList.add('content');
-
-    para.textContent = `${property}: ${book[property]}`
-
-    div.appendChild(para);
+  console.table(book);
+  for (let property in book) {
+    if (book.hasOwnProperty(property)) {
+      let para = document.createElement('p');
+      para.classList.add('content');
+      console.log(property);
+  
+      para.textContent = `${property}: ${book[property]}`
+  
+      div.appendChild(para);
+    }
+    
   }
 
   // add readStatusBtn to bottom
-  changeReadStatusBtn = document.createElement('input');
+  const changeReadStatusBtn = document.createElement('input');
   changeReadStatusBtn.type = 'button';
   changeReadStatusBtn.value = 'Change Read Status';
   changeReadStatusBtn.classList.add('read-btn');
   div.appendChild(changeReadStatusBtn);
 
   //add removeButton to bottom
-  removeButton = document.createElement('input');
+  const removeButton = document.createElement('input');
   removeButton.type = 'button';
   removeButton.value = 'Remove';
   removeButton.classList.add('remove-btn');
   div.appendChild(removeButton);
 
   // Append Div to book-container
-  bookContainer = document.querySelector('.book-container');
+  const bookContainer = document.querySelector('.book-container');
   bookContainer.appendChild(div);
 }
 
